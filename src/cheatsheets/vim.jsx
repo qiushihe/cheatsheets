8ed005a5e24e2b7f4b0d121dfd351cd203875378
Billy He <billy@metalabdesign.com>
2019-02-06 14:13:37 -0800

    Update cheatsheet with better symbols

--- src/cheatsheets/vim.jsx
+++ src/cheatsheets/vim.jsx
@@ -5,11 +5,12 @@ export const VimCheatSheet = () => (
   <Grid
     items={[
       [
-        null, null, null, null, null,
-        {colour: 'yellow', label: 'find word under cursor', keys: [{label: '#'}]},
-        {colour: 'yellow', label: 'previous \'text\'', keys: [{label: 'n'}]},
-        {colour: 'yellow', label: 'find \'text\'', keys: [{label: '?'}, {label: 'text', isInput: true}]},
-        {colour: 'yellow', label: 'next \'text\'', keys: [{label: 'N'}]}
+        null, null, null, null,
+        {colour: 'green', label: '🔍 word', cursor: 3, keys: [{label: '#'}]},
+        null,
+        {colour: 'yellow', label: '⇱ \'text\'', keys: [{label: 'n'}]},
+        {colour: 'yellow', label: '🔍 \'text\'', keys: [{label: '?'}, {label: 'text', isInput: true}]},
+        {colour: 'yellow', label: '⇲ \'text\'', keys: [{label: 'N'}]}
       ],
       [
         {colour: 'blue', label: 'first line', keys: [{label: 'g'}, {label: 'g'}]},
@@ -22,41 +23,41 @@ export const VimCheatSheet = () => (
       ],
       [
         null, null,
-        {colour: 'yellow', label: 'previous \'x\'', keys: [{label: ';'}]},
+        {colour: 'yellow', label: '⇤ \'x\'', keys: [{label: ';'}]},
         null,
-        {colour: 'green', label: 'WORD', keys: [{label: 'B'}]},
-        {colour: 'green', label: 'WORD', keys: [{label: 'g'}, {label: 'E'}]},
+        {colour: 'green', label: 'WORD', cursor: 0, keys: [{label: 'B'}]},
+        {colour: 'green', label: '·· WORD', cursor: 1, keys: [{label: 'g'}, {label: 'E'}]},
         null,
         {colour: 'green', label: 'up', keys: [{label: 'k'}]},
         null, null, null, null,
-        {colour: 'yellow', label: 'previous \'x\'', keys: [{label: ';'}]}
+        {colour: 'yellow', label: '⇤ \'x\'', keys: [{label: ','}]}
       ],
       [
-        {colour: 'blue', label: 'line', keys: [{label: '0'}]},
-        {colour: 'blue', label: 'non-blank', keys: [{label: '^'}]},
-        {colour: 'yellow', label: 'find \'x\'', keys: [{label: 'F'}, {label: 'x', isInput: true}]},
-        {colour: 'yellow', label: 'after \'x\'', keys: [{label: 'T'}, {label: 'x', isInput: true}]},
-        {colour: 'green', label: 'word', keys: [{label: 'b'}]},
-        {colour: 'green', label: 'word', keys: [{label: 'g'}, {label: 'e'}]},
+        {colour: 'blue', label: 'line', cursor: 0, keys: [{label: '0'}]},
+        {colour: 'blue', label: '__!blank', cursor: 2, keys: [{label: '^'}]},
+        {colour: 'yellow', label: '🔍 \'x\'', keys: [{label: 'F'}, {label: 'x', isInput: true}]},
+        {colour: 'yellow', label: '··x··', cursor: 3, keys: [{label: 'T'}, {label: 'x', isInput: true}]},
+        {colour: 'green', label: 'word', cursor: 0, keys: [{label: 'b'}]},
+        {colour: 'green', label: '·· word', cursor: 1, keys: [{label: 'g'}, {label: 'e'}]},
         {colour: 'green', label: 'left', keys: [{label: 'h'}]},
         null,
         {colour: 'green', label: 'right', keys: [{label: 'l'}]},
-        {colour: 'green', label: 'word', keys: [{label: 'e'}]},
-        {colour: 'green', label: 'word', keys: [{label: 'w'}]},
-        {colour: 'yellow', label: 'before \'x\'', keys: [{label: 't'}, {label: 'x', isInput: true}]},
-        {colour: 'yellow', label: 'find \'x\'', keys: [{label: 'f'}, {label: 'x', isInput: true}]},
-        {colour: 'blue', label: 'line', keys: [{label: '$'}]}
+        {colour: 'green', label: 'word', cursor: 3, keys: [{label: 'e'}]},
+        {colour: 'green', label: 'word ··', cursor: 5, keys: [{label: 'w'}]},
+        {colour: 'yellow', label: '··x··', cursor: 1, keys: [{label: 't'}, {label: 'x', isInput: true}]},
+        {colour: 'yellow', label: '🔍 \'x\'', keys: [{label: 'f'}, {label: 'x', isInput: true}]},
+        {colour: 'blue', label: 'line', cursor: 3, keys: [{label: '$'}]}
       ],
       [
         null, null,
-        {colour: 'yellow', label: 'next \'x\'', keys: [{label: ','}]},
+        {colour: 'yellow', label: '⇥ \'x\'', keys: [{label: ','}]},
         null, null, null, null,
         {colour: 'green', label: 'down', keys: [{label: 'j'}]},
         null,
-        {colour: 'green', label: 'WORD', keys: [{label: 'E'}]},
-        {colour: 'green', label: 'WORD', keys: [{label: 'W'}]},
+        {colour: 'green', label: 'WORD', cursor: 3, keys: [{label: 'E'}]},
+        {colour: 'green', label: 'WORD ··', cursor: 5, keys: [{label: 'W'}]},
         null,
-        {colour: 'yellow', label: 'next \'x\'', keys: [{label: ';'}]},
+        {colour: 'yellow', label: '⇥ \'x\'', keys: [{label: ';'}]},
       ],
       [
         {colour: 'blue', label: 'last line', keys: [{label: 'G'}]},
@@ -68,12 +69,13 @@ export const VimCheatSheet = () => (
         {colour: 'blue', label: 'paragraph', keys: [{label: '}'}]},
       ],
       [
-        {colour: 'blue', label: 'go to line #', keys: [{label: '#', isInput: true}, {label: 'G'}]},
+        {colour: 'yellow', label: 'go to line #', keys: [{label: '#', isInput: true}, {label: 'G'}]},
         null, null, null, null, null,
-        {colour: 'yellow', label: 'previous \'text\'', keys: [{label: 'N'}]},
-        {colour: 'yellow', label: 'find \'text\'', keys: [{label: '/'}, {label: 'text', isInput: true}]},
-        {colour: 'yellow', label: 'next \'text\'', keys: [{label: 'n'}]},
-        {colour: 'yellow', label: 'find word under cursor', keys: [{label: '*'}]}
+        {colour: 'yellow', label: '⇱ \'text\'', keys: [{label: 'N'}]},
+        {colour: 'yellow', label: '🔍 \'text\'', keys: [{label: '/'}, {label: 'text', isInput: true}]},
+        {colour: 'yellow', label: '⇲ \'text\'', keys: [{label: 'n'}]},
+        null,
+        {colour: 'green', label: '🔍 word', cursor: 3, keys: [{label: '*'}]}
       ]
     ]}
     itemComponent={KeyMap}
--- src/components/key-map.jsx
+++ src/components/key-map.jsx
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
+import flow from 'lodash/fp/flow';
 import map from 'lodash/fp/map';
+import isNumber from 'lodash/fp/isNumber';
+import isFinite from 'lodash/fp/isFinite';
+import runes from 'runes';
 
 import Key from '/src/components/key';
+
+const uncappedMap = map.convert({ cap: false });
+
+const isFiniteNumber = (val) => isNumber(val) && isFinite(val);
 
 const Base = styled.div`
   display: block;
@@ -12,7 +20,7 @@ const Base = styled.div`
 const InnerContent = styled.div`
   display: inline-block;
   border-radius: 3px;
-  box-shadow: 1px 1px 1px 1px #cccccc;
+  box-shadow: 1px 1px #cccccc;
   padding: 4px;
 
   ${({colour}) => colour === 'green' && `background-color: #dcf0dd;`}
@@ -31,8 +39,16 @@ const Keys = styled.div`
 const Label = styled.div`
   display: block;
   font-size: 12px;
-  font-family: sans-serif;
+  font-family: monospace;
   margin-top: 4px;
+  padding: 0 4px;
+`;
+
+const Character = styled.span``;
+
+const CharacterUnderCorsor = styled.span`
+  color: white;
+  background-color: black;
 `;
 
 export const KeyMap = ({
@@ -43,16 +59,31 @@ export const KeyMap = ({
       <div>&nbsp;</div>
     );
   } else {
-    const {label, keys, colour} = item;
+    const {label, keys, cursor, colour} = item;
     return (
       <Base>
         <InnerContent colour={colour}>
           <Keys>
-            {map(({label, isInput}) => (
-              <Key label={label} isInput={isInput} />
+            {map(({label: keyLabel, isInput}) => (
+              <Key label={keyLabel} isInput={isInput} />
             ))(keys)}
           </Keys>
-          <Label>{label}</Label>
+          <Label>
+            {isFiniteNumber(cursor) ? (
+              flow([
+                runes,
+                uncappedMap((char, index) => {
+                  const Komponent = index === cursor
+                    ? CharacterUnderCorsor
+                    : Character;
+
+                  return (
+                    <Komponent>{char}</Komponent>
+                  );
+                })
+              ])(label)
+            ) : label}
+          </Label>
         </InnerContent>
       </Base>
     );
